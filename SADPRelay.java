8bd7821f8cbb51dbae48afb110ec701049f6c245
Allan (Slam) <72238104+aayes89@users.noreply.github.com>
2025-10-15 23:17:51 -0600

    Update copyright information in SADPRelay.java

--- SADPRelay.java
+++ SADPRelay.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright 2025 Slam.
+ *  Copyright 2025 Allan (Slam).
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
